79ab403499f572d6ae92da671e927ae2243b8c1a
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2018-02-21 14:51:02 -1000

    Update 1

---
+++ js/generate.js
@@ -0,0 +1,3 @@
+var  fortuneOdds = ["Win 1 Million Dollars", "Free Date With Loved One", "Win a New Car 'Aston Martin'", 
+"Eat a cookie", "Graduate School", "Become Popular (to an extenet)", 
+"Obtain Friends (2 hrs)", "Receive Full Notes from Any Class Your Choose",];
