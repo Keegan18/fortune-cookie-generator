7e856c9a80314e1643eb4897d18864660d846786
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2018-02-23 14:42:03 -1000

    Upadate 2

--- js/generate.js
+++ js/generate.js
@@ -1,3 +1,8 @@
 var  fortuneOdds = ["Win 1 Million Dollars", "Free Date With Loved One", "Win a New Car 'Aston Martin'", 
 "Eat a cookie", "Graduate School", "Become Popular (to an extenet)", 
 "Obtain Friends (2 hrs)", "Receive Full Notes from Any Class Your Choose",];
+
+function generateFortuneCookie(){
+  var generator = math.floor(math.random() * fortuneOdds.length);
+  document.getElementById("display-fortune") = fortuneOdds[generator];
+}
